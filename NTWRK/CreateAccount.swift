bd2c92cfc0e34c53baa45122a129baa93ee7a7dd
david yash <dav1243@gmail.com>
2019-10-01 12:13:21 -0400

    probably will throw these changes out

---
+++ NTWRK/CreateAccount.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAccount.swift
+//  NTWRK
+//
+//  Created by Yashgur, David on 10/1/19.
+//  Copyright © 2019 David Yashgur. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccount: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
